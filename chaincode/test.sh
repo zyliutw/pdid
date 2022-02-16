703518ff3fe76dd01a0b28a1320ea9474095c159
Zi-Yuan Liu <yad50968@gmail.com>
2022-02-16 20:28:08 +0800

    Update test.sh

--- chaincode/test.sh
+++ chaincode/test.sh
@@ -1,73 +1,68 @@
-#!/bin/bash
+if [[ -z "${FPC_PATH}" ]]; then
+  echo "Error: FPC_PATH not set"; exit 1
+fi
 
-SCRIPTDIR="$(dirname $(readlink --canonicalize ${BASH_SOURCE}))"
-FPC_TOP_DIR="${SCRIPTDIR}/../.."
-FABRIC_CFG_PATH="${SCRIPTDIR}/../../integration/config"
-FABRIC_SCRIPTDIR="${FPC_TOP_DIR}/fabric/bin/"
+FABRIC_CFG_PATH="${FPC_PATH}/integration/config"
+FABRIC_SCRIPTDIR="${FPC_PATH}/fabric/bin/"
 
 . ${FABRIC_SCRIPTDIR}/lib/common_utils.sh
 . ${FABRIC_SCRIPTDIR}/lib/common_ledger.sh
 
-CC_ID=GPM_test
+# this is the path points to FPC chaincode binary
+CC_PATH=${FPC_PATH}/samples/chaincode2/_build/lib/
 
-#this is the path that will be used for the docker build of the chaincode enclave
-ENCLAVE_SO_PATH=examples/chaincode/_build/lib/
+CC_ID=helloworld_test
+CC_VER="$(cat ${CC_PATH}/mrenclave)"
+CC_EP="OR('SampleOrg.member')"
+CC_SEQ="1"
 
-CC_VERS=0
+run_test() {
+    
 
-GPM_test() {
-    say "- do hello world"
-    # install and instantiate GPM  chaincode
+    say "- install helloworld chaincode"
+    PKG=/tmp/${CC_ID}.tar.gz
+    ${PEER_CMD} lifecycle chaincode package --lang fpc-c --label ${CC_ID} --path ${CC_PATH} ${PKG}
+    ${PEER_CMD} lifecycle chaincode install ${PKG}
 
-    # builds the docker image; creates the docker container and enclave;
-    # input:  CC_ID:chaincode name; CC_VERS:chaincode version;
-    #         ENCLAVE_SO_PATH:path to build artifacts
+    PKG_ID=$(${PEER_CMD} lifecycle chaincode queryinstalled | awk "/Package ID: ${CC_ID}/{print}" | sed -n 's/^Package ID: //; s/, Label:.*$//;p')
 
-    say "- install GPM chaincode"
-    try ${PEER_CMD} chaincode install -l fpc-c -n ${CC_ID} -v ${CC_VERS} -p ${ENCLAVE_SO_PATH}
-    sleep 3
-
-    # instantiate GPM chaincode
-    say "- instantiate GPM chaincode"
-    try ${PEER_CMD} chaincode instantiate -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -v ${CC_VERS} -c '{"args":["init"]}' -V ecc-vscc
-    sleep 3
+    ${PEER_CMD} lifecycle chaincode approveformyorg -o ${ORDERER_ADDR} -C ${CHAN_ID} --package-id ${PKG_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+    ${PEER_CMD} lifecycle chaincode checkcommitreadiness -C ${CHAN_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+    ${PEER_CMD} lifecycle chaincode commit -o ${ORDERER_ADDR} -C ${CHAN_ID} --name ${CC_ID} --version ${CC_VER} --sequence ${CC_SEQ} --signature-policy ${CC_EP}
+    
+    ${PEER_CMD} lifecycle chaincode initEnclave -o ${ORDERER_ADDR} --peerAddresses "localhost:7051" --name ${CC_ID}
 
     for i in {1..1}
     do
         say "- Register PDID"
-        try ${PEER_CMD} chaincode invoke -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -c ${REQ_newPDID} --waitForEvent
+        try ${PEER_CMD} chaincode invoke -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -c ${REQ_newPDID} -C ${CHAN_ID} --waitForEvent
     done
 
     for i in {1..1}
     do
         say " - Auth via PDID"
-        try ${PEER_CMD} chaincode invoke -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -c ${REQ_authPDID} --waitForEvent
+        try ${PEER_CMD} chaincode invoke -o ${ORDERER_ADDR} -C ${CHAN_ID} -n ${CC_ID} -c ${REQ_authPDID} -C ${CHAN_ID} --waitForEvent
     done
 
 }
 
-# 1. prepare
-para
-say "Preparing GPM Test ..."
-# - clean up relevant docker images
-docker_clean ${ERCC_ID}
-docker_clean ${CC_ID}
-
 trap ledger_shutdown EXIT
 
-para
-say "Run GPM  test"
-
-say "- setup ledger"
+say "Setup ledger ..."
 ledger_init
 
-say "- GPM test"
-GPM_test
-
-say "- shutdown ledger"
-ledger_shutdown
+para
+say "Run helloworld test ..."
+run_test
 
 para
-yell "GPM test PASSED"
+say "Shutdown ledger ..."
+ledger_shutdown
+
+yell "Helloworld test PASSED"
 
 exit 0
+
+
+
+
